bbe2312414ad9673841c7be69ebbc5b75d6d2940
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-01-01 15:42:43 +0530

    Add files via upload

---
+++ Frequency-Array-Retrieval.cpp
@@ -0,0 +1,48 @@
+// problem code -> FREQARRRET
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T, N, remN;
+    cin >> T;
+    while (T--)
+    {
+        int i, count, asgnVal = 0;
+        cin >> N;
+        if (N <= 0)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        remN = N;
+        int arr[N], resArr[N] = {0};
+        for (i = 0; i < N; i++)
+            cin >> arr[i];
+        for (i = 0; (i < N) && (arr[i] <= remN); i++)
+        {
+            count = 1;
+            if (arr[i] == -1)
+                continue;
+            resArr[i] = ++asgnVal;
+            for (int j = i + 1; (j < N) && (count < arr[i]); j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                    arr[j] = -1;
+                    resArr[j] = asgnVal;
+                }
+            }
+            (count == arr[i]) ? remN -= arr[i] : remN;
+        }
+        if (remN == 0)
+            for (i = 0; i < N; i++)
+                cout << resArr[i] << " ";
+        else
+            cout << -1;
+        cout << endl;
+    }
+    return 0;
+}
