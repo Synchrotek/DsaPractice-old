8d4c231c0281463c11c1821db39652f6e9716c6e
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-01-15 13:12:30 +0530

    Add files via upload

---
+++ 2DArray-DS.cpp
@@ -0,0 +1,34 @@
+// Topic -> Data Structure (Array)
+// Language -> C++
+// site -> hackerrank.com
+// problemName -> 2D Array - DS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Nrow, Ncolumn;
+    Nrow = 4, Ncolumn = 4;
+    int i, j;
+    int currentSum, resSum;
+    int arr[Nrow][Ncolumn];
+
+    for (i = 0; i < Nrow; i++)
+        for (j = 0; j < Ncolumn; j++)
+            cin >> arr[i][j];
+
+    i = j = 0;
+    while (i < Nrow - 2)
+    {
+        currentSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+        currentSum += arr[i + 1][j + 1];
+        currentSum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+        ((i == 0) && (j == 0)) ? resSum = currentSum : resSum;
+        (currentSum >= resSum) ? resSum = currentSum : resSum;
+        (j >= Ncolumn - 3) ? i++, j = 0 : j++;
+    }
+    cout << resSum << endl;
+    return 0;
+}
