efd6dd3d58e4d5c76903f7f52235d9bc2e0c9c78
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-04-23 08:15:54 +0530

    Update Dynamic-Array.cpp

--- Dynamic-Array.cpp
+++ Dynamic-Array.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 using namespace std;
 
-void appendValue(int *&arr, int size23, int value)
+void appendValue(int *&arr, int size, int value)
 {
     int *newArray = new int[size + 1];
     for (int i = 0; i < size; i++)
