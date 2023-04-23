dc2ced8ae4e19db3891091893525836dd4b108ee
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-04-23 08:16:02 +0530

    Update Dynamic-Array.cpp

--- Dynamic-Array.cpp
+++ Dynamic-Array.cpp
@@ -11,7 +11,7 @@ void appendValue(int *&arr, int size, int value)
     int *newArray = new int[size + 1];
     for (int i = 0; i < size; i++)
         newArray[i] = arr[i];
-    newArray[size] = value;
+    newArray[size] = value67;
     delete[] arr;
     arr = newArray;
     newArray = nullptr;
