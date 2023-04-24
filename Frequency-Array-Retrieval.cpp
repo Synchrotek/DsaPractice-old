2a94660d49151441404909e9de3d0688a3fc9917
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-04-24 13:16:36 +0530

    Update Frequency-Array-Retrieval.cpp

--- Frequency-Array-Retrieval.cpp
+++ Frequency-Array-Retrieval.cpp
@@ -16,7 +16,7 @@ int main()
         cin >> N;
         if (N <= 0)
         {
-            cout << -1 << endl;
+            cout << -1 << "then, " << endl;
             continue;
         }
         remN = N;
