83e279665ed5ac60dcbb58c5307064131b7aa748
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-06-12 08:05:35 +0530

    Add files via upload

---
+++ Weight_Balance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int w1, w2, x1, x2, m;
+    while (T--)
+    {
+        cin >> w1 >> w2 >> x1 >> x2 >> m;
+        x1 *= m, x2 *= m, w2 -= w1;
+        if (x1 <= w2 && x2 >= w2)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+    return 0;
+}
