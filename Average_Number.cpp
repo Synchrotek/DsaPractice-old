2cefc3044ca029970126a606c9af4544a10cb789
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-07-02 04:45:10 +0530

    Create Average_Number.cpp

---
+++ Average_Number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, K, V, sum = 0, diff;
+        cin >> N >> K >> V;
+        for (int i = 0; i < N; i++)
+            cin >> diff, sum += diff;
+
+        diff = V * (N + K) - sum;
+
+        if ((diff > 0) && (diff % K == 0))
+            cout << diff / K << endl;
+        else
+            cout << -1 << endl;
+    }
+
+    return 0;
+}
