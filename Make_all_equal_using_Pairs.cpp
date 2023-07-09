b4242795b8e0bc66bcdc55b3f36987982af8b828
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-07-09 06:04:26 +0530

    Update Make_all_equal_using_Pairs.cpp

--- Make_all_equal_using_Pairs.cpp
+++ Make_all_equal_using_Pairs.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <map>
+using namespace std;
 
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int i, res = 0, arr[N];
+        map<int, int> numFreq;
+
+        for (i = 0; i < N; i++)
+            cin >> arr[i], numFreq[arr[i]] = 0;
+
+        for (i = 0; i < N; i++)
+            numFreq[arr[i]]++;
+
+        map<int, int>::iterator itr;
+        for (itr = numFreq.begin(); itr != numFreq.end(); itr++)
+            ((*itr).second > res) ? res = (*itr).second : res;
+
+        cout << (N - res) << endl;
+    }
+
+    return 0;
+}
