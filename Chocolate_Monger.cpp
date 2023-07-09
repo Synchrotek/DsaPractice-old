a930a1c7ca55872c23246b9f384ac4c4e6eb056b
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-07-09 06:06:22 +0530

    Create Chocolate_Monger.cpp

---
+++ Chocolate_Monger.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, X;
+        cin >> N >> X;
+        int arr[N], dplCnt = 0;
+
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+
+        sort(arr, arr + N);
+
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] != arr[i + 1])
+                dplCnt++;
+        }
+        X = N - X;
+        if (dplCnt < X)
+            cout << dplCnt << endl;
+        else
+            cout << X << endl;
+    }
+
+    return 0;
+}
