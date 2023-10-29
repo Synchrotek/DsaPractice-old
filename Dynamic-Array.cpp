49b91c8f4e225df9ae63e95985bc2b5ff4b2eaf8
Satyajit Nayak <77431114+Synchrotek@users.noreply.github.com>
2023-10-29 05:38:02 +0530

    Update Dynamic-Array.cpp

--- Dynamic-Array.cpp
+++ Dynamic-Array.cpp
@@ -1,7 +1,7 @@
 // Topic -> Data Structure (Array)
 // Language -> C++
 // site -> hackerrank.com
-// problemName -> Dynamic Array Problem
+// problemName -> Dynamic Array DS Problem
 
 #include <iostream>
 using namespace std;
