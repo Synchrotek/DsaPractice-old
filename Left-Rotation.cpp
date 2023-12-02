4549a54c54810fd05072bfb0602ad8c7a44b9140
Satyajit Nayak <77431114+Synchrotek@users.noreply.github.com>
2023-12-02 07:52:00 +0530

    Update Left-Rotation.cpp

--- Left-Rotation.cpp
+++ Left-Rotation.cpp
@@ -1,7 +1,7 @@
 // Topic -> Data Structure (Array)
 // Language -> C++
 // site -> hackerrank.com
-// problemName -> Left Rotation
+// problemName -> Left
 
 #include <iostream>
 using namespace std;
