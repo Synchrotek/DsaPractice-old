f1a956c76bfdbffd353c6a3a4436a9761a76d647
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-11 17:02:46 +0530

    quickPracticeS0rt

--- S0rt1ng.cpp
+++ S0rt1ng.cpp
@@ -1,66 +1,49 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int l, int mid, int r)
+void swap(int arr[], int i, int j)
 {
-    int n1 = mid - l + 1;
-    int n2 = r - mid;
-
-    int a[n1], b[n2];
-    for (int i = 0; i < n1; i++)
-        a[i] = arr[l + i];
-    for (int i = 0; i < n2; i++)
-        b[i] = arr[mid + 1 + i];
-
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2)
-    {
-        if (a[i] < b[j])
-        {
-            arr[k] = a[i];
-            k++, i++;
-        }
-        else
-        {
-            arr[k] = b[j];
-            k++, j++;
-        }
-
-        while (i < n1)
-        {
-            arr[k] = a[i];
-            k++, i++;
-        }
-        while (j < n2)
-        {
-            arr[k] = b[j];
-            k++, j++;
-        }
-    }
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-void mergeSort(int arr[], int l, int r)
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, r);
+    return i + 1;
+}
+
+void quickSort(int arr[], int l, int r)
 {
     if (l < r)
     {
-        int mid = (l + r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-
-        merge(arr, l, mid, r);
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, r);
     }
 }
 
 int main()
 {
-    int N;
-    cin >> N;
-    int arr[N];
-    for (int i = 0; i < N; i++)
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    mergeSort(arr, 0, N - 1);
-    for (int i = 0; i < N; i++)
+    quickSort(arr, 0, 4);
+    for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 
