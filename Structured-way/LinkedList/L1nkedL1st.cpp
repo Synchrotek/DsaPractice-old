090dc13daa779e2a420369051e50bd8f05e75200
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-12 17:51:24 +0530

    L1nkedL1st

--- Structured-way/LinkedList/L1nkedL1st.cpp
+++ Structured-way/LinkedList/L1nkedL1st.cpp
@@ -14,6 +14,7 @@ public:
     }
 };
 
+// Insertion Methods ------------------------------
 void insertAtTail(node *&head, int val)
 {
     node *n = new node(val);
@@ -30,7 +31,6 @@ void insertAtTail(node *&head, int val)
     }
     temp->next = n;
 }
-
 void insertAtHead(node *&head, int val)
 {
     node *n = new node(val);
@@ -38,6 +38,58 @@ void insertAtHead(node *&head, int val)
     head = n;
 }
 
+// Deletion Methods ------------------------------
+void deleteAtHead(node *&head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    node *toDeleteNode = head;
+    head = head->next;
+    delete toDeleteNode;
+}
+void deletionByEle(node *&head, int target)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    if (head->data == target)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node *temp = head;
+    while (temp->next->data != target)
+    {
+        temp = temp->next;
+    }
+    node *toDeleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete toDeleteNode;
+}
+
+// Searching Method ------------------------------
+void searchElement(node *&head, int target)
+{
+    node *temp = head;
+    int index = 0;
+    while (temp != NULL)
+    {
+        if (temp->data == target)
+        {
+            cout << target << " Found at: "
+                 << index << endl;
+            return;
+        }
+        index++;
+        temp = temp->next;
+    }
+    cout << target << ": not found " << endl;
+}
+
+// Traversal Method -------------------------------
 void display(node *head)
 {
     node *temp = head;
@@ -59,6 +111,11 @@ int main()
     insertAtHead(head, 16);
     insertAtHead(head, 6);
 
+    display(head);
+    searchElement(head, 6);
+
+    deletionByEle(head, 24);
+    // deleteAtHead(head);
     display(head);
 
     return 0;
