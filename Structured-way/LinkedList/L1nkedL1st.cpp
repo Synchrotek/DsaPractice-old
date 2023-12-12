70997229ebb0007c509270373cd371ff282a0b64
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-12 17:25:05 +0530

    L1nkedL1st

---
+++ Structured-way/LinkedList/L1nkedL1st.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 12);
+    insertAtTail(head, 24);
+    insertAtTail(head, 42);
+
+    insertAtHead(head, 16);
+    insertAtHead(head, 6);
+
+    display(head);
+
+    return 0;
+}
